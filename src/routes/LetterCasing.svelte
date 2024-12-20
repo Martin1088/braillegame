b4a0253b5c8c2a0e3e4f39d87aa123e4748429ac
Martin Jurk <m049k@meinschatz.fritz.box>
2024-12-20 16:02:20 +0100

    letter spone

---
+++ src/routes/LetterCasing.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+  import { onMount } from "svelte";
+
+  let inputValue: string;
+  let message: string;
+  async function updateContent() {
+    try {
+      // Call the Tauri command to get a random character
+      message = await invoke<string>("dynamic");
+    } catch (error) {
+      console.error("Error updating content:", error);
+    }
+  }
+
+  // Set up the interval to update content every 10 seconds
+  onMount(() => {
+    const interval = setInterval(updateContent, 10000);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  });
+</script>
+
+<div class="d-flex flex-row">
+  <div class="card mx-auto">
+    <div class="card-body">
+      <h3 class="card-title">type the anouced letter</h3>
+      <div class="mb-3">
+        <label for="inputField" class="form-label">Enter your text:</label>
+        <input
+          id="inputField"
+          type="text"
+          class="form-control"
+          bind:value={inputValue}
+          aria-describedby="inputHelp"
+        />
+      </div>
+      <div aria-live="assertive" role="alert" class="visually-hidden">
+        <p>Your random letter is ready:</p>
+        <pre>{message}</pre>
+      </div>
+    </div>
+  </div>
+</div>
